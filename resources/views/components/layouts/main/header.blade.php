6be5d39c6671a37f96c5e51d2b200c9981237d72
Abdur-Rahman Saad <abdos1166@gmail.com>
2025-05-07 01:33:27 +0300

    Search Modal

--- resources/views/components/layouts/main/header.blade.php
+++ resources/views/components/layouts/main/header.blade.php
@@ -14,9 +14,9 @@
             </a>
         </div>
         <div class="flex items-center gap-3">
-            <a href="#">
+            <button class="cursor-pointer" aria-haspopup="dialog" aria-expanded="false" aria-controls="hs-vertically-centered-modal" data-hs-overlay="#hs-vertically-centered-modal">
                 <x-heroicon-o-magnifying-glass class="size-6"/>
-            </a>
+            </button>
             <a href="#">
                 <x-heroicon-o-shopping-bag class="size-6"/>
             </a>
@@ -32,3 +32,33 @@
         </ul>
     </nav>
 </header>
+
+<div id="hs-vertically-centered-modal" class="hs-overlay hidden size-full fixed top-0 start-0 z-80 overflow-x-hidden overflow-y-auto pointer-events-none" role="dialog" tabindex="-1" aria-labelledby="hs-vertically-centered-modal-label">
+    <div class="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto min-h-[calc(100%-56px)] flex items-center">
+        <div class="w-full flex flex-col bg-white border border-gray-200 shadow-2xs rounded-xl pointer-events-auto">
+            <div class="flex justify-between items-center py-3 px-4 border-b border-gray-200">
+                <h3 id="hs-vertically-centered-modal-label" class="font-bold text-gray-800">
+                    البحث
+                </h3>
+                <button type="button" class="cursor-pointer size-8 inline-flex justify-center items-center gap-x-2 rounded-full border border-transparent bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-hidden focus:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none" aria-label="Close" data-hs-overlay="#hs-vertically-centered-modal">
+                    <span class="sr-only">إغلاق</span>
+                    <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M18 6 6 18"></path>
+                        <path d="m6 6 12 12"></path>
+                    </svg>
+                </button>
+            </div>
+            <div class="p-4 overflow-y-auto">
+                <input type="text" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-slate-500 focus:ring-slate-500" placeholder="باقة ورود..." autofocus="">
+            </div>
+            <div class="flex justify-end items-center gap-x-2 py-3 px-4 border-t border-gray-200">
+                <button type="button" class="cursor-pointer py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-2xs hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none" data-hs-overlay="#hs-vertically-centered-modal">
+                    إغلاق
+                </button>
+                <button type="button" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-slate-600 text-white hover:bg-slate-700 focus:outline-hidden focus:bg-slate-700 disabled:opacity-50 disabled:pointer-events-none cursor-pointer">
+                    بحث
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
