42a2d15d8d4bfc4f00ab12af596c9e94641f907b
Abdur-Rahman Saad <abdos1166@gmail.com>
2025-05-08 08:17:38 +0300

    Product Filament Resource

---
+++ app/Filament/Resources/ProductResource.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductResource\Pages;
+use App\Models\Product;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $slug = 'products';
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->label('الاسم')
+                    ->required(),
+
+                TextInput::make('price')
+                    ->label('السعر')
+                    ->required()
+                    ->numeric(),
+
+                TextInput::make('discount')
+                    ->label('التخفيض')
+                    ->numeric(),
+
+                Select::make('discount_type')
+                    ->label('نوع التخفيض')
+                    ->native(false)
+                    ->options([
+                        'fixed' => 'ثابت',
+                        'percentage' => 'نسبة مئوية',
+                    ]),
+
+                TextInput::make('quantity')
+                    ->label('الكمية')
+                    ->required()
+                    ->integer(),
+
+                Select::make('categories')
+                    ->relationship('categories', 'name')
+                    ->multiple()
+                    ->preload()
+                    ->label('الأقسام')
+                    ->searchable(),
+
+                SpatieMediaLibraryFileUpload::make('الصور')
+                    ->reorderable()
+                    ->openable()
+                    ->appendFiles()
+                    ->columnSpanFull()
+                    ->multiple()->panelLayout('grid'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('الاسم')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('price')->label('السعر')->sortable(),
+
+                TextColumn::make('discount')->label('التخفيض'),
+
+                TextColumn::make('discount_type')
+                    ->label('نوع التخفيض')
+                    ->formatStateUsing(fn($state) => $state === 'fixed' ? 'ثابت' : ($state === 'percentage' ? 'نسبة مئوية' : $state)),
+
+                TextColumn::make('quantity')->label('الكمية')->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->bulkActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getGloballySearchableAttributes(): array
+    {
+        return ['name', 'slug'];
+    }
+
+    public static function getNavigationLabel(): string
+    {
+        return 'المنتجات';
+    }
+}
