26566ed532f42fb34285e07e88d6eb5840db8aaf
Abdur-Rahman Saad <abdos1166@gmail.com>
2025-05-17 23:30:49 +0300

    =Cart Empty State UI

--- resources/views/livewire/cart/cart-summary.blade.php
+++ resources/views/livewire/cart/cart-summary.blade.php
@@ -1,37 +1,44 @@
 <div class="grid grid-cols-1 xl:grid-cols-4 gap-5">
-    <div class="xl:col-span-3 flex flex-col gap-5">
-        @foreach($cartItems as $item)
-            <livewire:cart.cart-item :item="$item" :key="$item->id"/>
-        @endforeach
-    </div>
-    <div>
-        <div class="border rounded-2xl p-7 flex flex-col gap-5 sticky top-5">
-            <h4 class="text-xl font-semibold text-slate-700">ملخص الطلب</h4>
-            <div class="flex justify-between">
-                <span class="text-slate-600">مجموع المنتجات</span>
-                <span class="flex items-center gap-2 font-semibold">{{ $summary['subtotal'] }} @include('components.layouts.riyal-icon', ['class' => 'size-4 fill-slate-900'])</span>
-            </div>
-            <div class="flex justify-between">
-                <span class="text-slate-600">التوصيل</span>
-                <span class="flex items-center gap-2 font-semibold">{{ $summary['shippingCost'] }} @include('components.layouts.riyal-icon', ['class' => 'size-4 fill-slate-900'])</span>
-            </div>
-            <div class="flex justify-between">
-                <span class="text-slate-600">الإجمالي</span>
-                <span class="flex items-center gap-2 font-semibold">{{ $summary['total'] }} @include('components.layouts.riyal-icon', ['class' => 'size-4 fill-slate-900'])</span>
-            </div>
-            @if(!$allItemsValid)
-                <span class="text-red-600">يرجى إكمال بيانات الطلبات</span>
-            @endif
-            <button wire:click="checkout" class="flex justify-center items-center bg-slate-800 py-3 rounded-full text-white font-bold cursor-pointer
+    @if($cartItems->count())
+        <div class="xl:col-span-3 flex flex-col gap-5">
+            @foreach($cartItems as $item)
+                <livewire:cart.cart-item :item="$item" :key="$item->id"/>
+            @endforeach
+        </div>
+        <div>
+            <div class="border rounded-2xl p-7 flex flex-col gap-5 sticky top-5">
+                <h4 class="text-xl font-semibold text-slate-700">ملخص الطلب</h4>
+                <div class="flex justify-between">
+                    <span class="text-slate-600">مجموع المنتجات</span>
+                    <span class="flex items-center gap-2 font-semibold">{{ $summary['subtotal'] }} @include('components.layouts.riyal-icon', ['class' => 'size-4 fill-slate-900'])</span>
+                </div>
+                <div class="flex justify-between">
+                    <span class="text-slate-600">التوصيل</span>
+                    <span class="flex items-center gap-2 font-semibold">{{ $summary['shippingCost'] }} @include('components.layouts.riyal-icon', ['class' => 'size-4 fill-slate-900'])</span>
+                </div>
+                <div class="flex justify-between">
+                    <span class="text-slate-600">الإجمالي</span>
+                    <span class="flex items-center gap-2 font-semibold">{{ $summary['total'] }} @include('components.layouts.riyal-icon', ['class' => 'size-4 fill-slate-900'])</span>
+                </div>
+                @if(!$allItemsValid)
+                    <span class="text-red-600">يرجى إكمال بيانات الطلبات</span>
+                @endif
+                <button wire:click="checkout" class="flex justify-center items-center bg-slate-800 py-3 rounded-full text-white font-bold cursor-pointer
               {{ $allItemsValid ? 'hover:bg-slate-900' : 'opacity-50 cursor-not-allowed' }} transition"
                     {{ $allItemsValid ? '' : 'disabled' }}>
-                <div wire:loading class="animate-spin inline-block size-6 border-3 border-current border-t-transparent text-white rounded-full" role="status" aria-label="loading">
-                    <span class="sr-only">Loading...</span>
-                </div>
-                <span wire:loading.remove>
+                    <div wire:loading class="animate-spin inline-block size-6 border-3 border-current border-t-transparent text-white rounded-full" role="status" aria-label="loading">
+                        <span class="sr-only">Loading...</span>
+                    </div>
+                    <span wire:loading.remove>
                     إتمام الطلب
                 </span>
-            </button>
+                </button>
+            </div>
         </div>
-    </div>
+    @else
+        <div class="rounded-xl border justify-center items-center flex flex-col gap-5 h-96 mt-10 col-span-full">
+            <x-heroicon-o-shopping-cart class="size-16 text-slate-400"/>
+            <h3 class="text-slate-400 font-semibold text-xl">لا توجد منتجات في العربة</h3>
+        </div>
+    @endif
 </div>
